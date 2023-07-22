60672f05f7df424cd9c3ab4fcfc8b43c9520ed2d
farshadalemi <farshad.alemi@gmail.com>
2023-07-22 13:46:22 +0330

    fun and key added

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,52 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [newItem, setNewItem] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  function handelSubmit(e) {
+    e.preventDefault();
+
+    setTodos((currentTodos) => {
+      return;
+      [
+        ...currentTodos,
+        { id: crypto.randomUUID, title: newItem, completed: false },
+      ];
+    });
+    setNewItem("");
+  }
+
   return (
     <>
-      <form className="new-item-form" action="">
+      <form onSubmit={handelSubmit} className="new-item-form" action="">
         <h1>TODO LIST</h1>
         <div className="form-row">
           <label htmlFor="item">✍️</label>
-          <input type="text" id="item" placeholder="Type here..." />
+          <input
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+            type="text"
+            id="item"
+            placeholder="Type here..."
+          />
         </div>
         <button className="btn">Add</button>
       </form>
       <h2 className="header">My list</h2>
       <ul className="list">
-        <li>
-          <label htmlFor="">
-            <input type="checkbox" />
-            Item 1<button className="btn done-btn">Done</button>
-          </label>
-        </li>
-
-        <li>
-          <label htmlFor="">
-            <input type="checkbox" />
-            Item 2<button className="btn done-btn">Done</button>
-          </label>
-        </li>
+        {todos.map((todo) => {
+          return (
+            <li key={todo.id}>
+              <label htmlFor="">
+                <input type="checkbox" checked={todo.completed} />
+                {todo.title}
+                <button className="btn done-btn">Done</button>
+              </label>
+            </li>
+          );
+        })}
       </ul>
     </>
   );
